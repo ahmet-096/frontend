35098d9a26abe711c750ac58aaf71f5d2b1138b2
ahmet-096 <bagaclibertug@gmail.com>
2025-08-21 09:49:12 +0300

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-colfadsfsf items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-colsadfasffsfadsfsf items-center justify-center min-h-screen bg-gray-100">
     </div>
   );
 }
